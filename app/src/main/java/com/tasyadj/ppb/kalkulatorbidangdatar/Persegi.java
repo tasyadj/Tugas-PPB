e29bb6e86d21d4e0a5d1068a4a45ce05602b3ddc
tasyadj <tasyadj0903@gmail.com>
2021-10-19 14:25:11 +0700

    kalkulator bidang datar

---
+++ app/src/main/java/com/tasyadj/ppb/kalkulatorbidangdatar/Persegi.java
@@ -0,0 +1,12 @@
+package com.tasyadj.ppb.kalkulatorbidangdatar;
+
+public class Persegi {
+
+    Double keliling(double panjang){
+        return 4*panjang;
+    }
+
+    Double luas(double panjang, double lebar){
+        return panjang*lebar;
+    }
+}
---
+++ app/src/main/java/com/tasyadj/ppb/kalkulatorbidangdatar/Segitiga.java
@@ -0,0 +1,12 @@
+package com.tasyadj.ppb.kalkulatorbidangdatar;
+
+public class Segitiga {
+
+    Double keliling(double panjang){
+        return 3*panjang;
+    }
+
+    Double luas(double alas, double tinggi){
+        return (alas*tinggi)/2;
+    }
+}
